445134864a3bbc55853226eb338f9e217f1a5d1e
TemirovaAzima <temirovaazima9@gmail.com>
2025-03-30 19:39:17 +0500

    Home and images added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Controls from "./components/Controls.jsx";
 import {useState} from "react";
 import Navbar from "./components/Navbar.jsx";
+import Home from "./components/Home.jsx";
 
 export const App = () => {
     const [frameZoom, setFrameZoom] = useState(false);
@@ -17,13 +18,16 @@ export const App = () => {
     return (
         <div className="w-full h-screen grid place-items-center">
             <div className={`${frameZoom ? 'min-w-[97vw] min-h-[97vh]' : 'min-w-[70vw] min-h-[85vh]'}
-            w-[70vw] h-[85vh]  max-w-[90vw]max-h-[90vh] 
+            w-[70vw] h-[85vh]  max-w-[90vw] max-h-[90vh] 
             border border-gray-300 rounded-2xl resize overflow-auto relative
-            transition-all duration-300
+            transition-all duration-300 flex 
                 "`}
             >
                 <Controls toggleZoom={toggleZoom} frameZoom={frameZoom}/>
                 <Navbar activePage={activePage} handleNavClick={handleNavClick}/>
+                <div className={'grow'}>
+                    <Home/>
+                </div>
             </div>
         </div>
     )
